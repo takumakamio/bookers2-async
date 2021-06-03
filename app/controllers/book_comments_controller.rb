54b4d34b3b425c299b4e03952d1359ad327893cf
EC2 Default User <ec2-user@ip-172-31-19-107.ec2.internal>
2021-06-03 05:19:58 +0000

    “FIX”

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -3,11 +3,11 @@ class BookCommentsController < ApplicationController
     before_action :authenticate_user!
 
     def create
-        @book = Book.find(params[:book_id]) #コメントする投稿id
+        @book = Book.find(params[:book_id])
         #@book_comment = current_user.book_comments.new(book_comment_params)に省略できる。 誰が投稿、コメントのデータ
         @comment = BookComment.new(book_comment_params)
         @comment.user_id = current_user.id
-        @comment.book_id = @book.id #どの投稿にコメントするか
+        @comment.book_id = @book.id 
         @comment_new = BookComment.new
         if @comment.save
         else
